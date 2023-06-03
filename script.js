7d1313ff1fe7e45ed89b2a8f6788da5f4a388184
yoshiko <ttkmysk021@gmail.com>
2023-06-03 11:28:38 +0900

    second

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// 1
+// Q1 プロパティーとメソッドの違いをまとめて3つほどメソッドを作ってください。
+
   //プロパティは名前と値がセットになっているもの。変数のようなイメージで、属性のこと。。メソッドとはプロパティに代入された関数のことをいいます。動作のことを言います。
   // 例)const phone = {
   // maker    :"apple",→プロパティ
@@ -14,16 +15,19 @@
   // function prompt();
   // function click();
 
-  // 2
+
+  // Q2 下記の関数を実行してその結果をコンソールに表示してください。(スコープ内に書く方法はだめです。
+
   function fruit(name, price) {
     const result = name + "の値段は" + price + "円です。"
     return result
   };
-  var resultReturn = fruit("りんご", "100")
+  var resultReturn = fruit("Q2 りんご", "100")
   console.log(resultReturn);
   
 
-  // 3
+  // Q3　下記の関数を経由して2つの関数を実行するような処理を作ってください。
+
   function addTax(price, func) {
 // priceは値段
 // funcは実行する関数名
@@ -32,13 +36,20 @@
     func(taxPrice, price);
   }
 // トマトの税込み価格をコンソールに表示させる処理
-    const tomato =function (taxPrice, price){
-    console.log(taxPrice);
+  { const tomato =function (taxPrice, price){
+    console.log("Q3 オニオンの値段は" +  taxPrice + "円");
+    return false;
     }
     addTax(120, tomato);
+  }
 
 // 玉ねぎの税込み価格をコンソールに表示させる処理
     const onion =function (taxPrice, price){
-    console.log(taxPrice);
+    console.log("Q3 玉ねぎの値段" + taxPrice + "円");
+    return false;
     }
     addTax(115, onion);
+    
+    
+
+    
